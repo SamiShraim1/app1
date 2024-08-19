0d4bd43c9a8ed71d896d305e9d40931006124847
SamiShraim1 <samialnser@gmail.com>
2024-08-19 20:50:23 +0300

    Add project files.

---
+++ app1/Controllers/StudentsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace app1.Controllers
+{
+    public class StudentsController : Controller
+    {
+        public IActionResult getAll()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public IActionResult Delete()
+        {
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+    }
+}
